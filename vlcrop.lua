6cec5c6ff4e991a13f60d101cf59d70c97683542
Alin Popa <alin.popa@gmail.com>
2024-04-27 16:02:33 +0100

    Init.

---
+++ vlcrop.lua
@@ -0,0 +1,9 @@
+function descriptor()
+  return {
+    title = "VLCrop 0.0.1",
+    version = "0.0.1",
+    author = "Alin Popa",
+    url = 'https://github.com/alinpopa/vlcrop',
+    shortdesc = "VLC Crop current video.",
+  }
+end
